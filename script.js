e7f75f838d6dab02fd8c7ce1276509f1c38b39bc
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-23 16:00:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const counterdisplay=document.getElementById("counter");
+		const incrementbutton=document.getElementById("incrementBtn");
+			let count=0;
+		incrementbutton.addEventListener("click",function(){
+			alert("Before increment: " + count);
+			count++;
+			counterdisplay.textContent=count;	
+		});
